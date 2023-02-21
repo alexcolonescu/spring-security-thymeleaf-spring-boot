eb94c9de6a47a00366842d931677f8245cf26986
alex <colo.alexandru@yahoo.com>
2023-02-21 13:48:49 +0200

    Initial commit

---
+++ src/main/java/com/example/spring/security/thymeleaf/spring/boot/SpringSecurityThymeleafSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.security.thymeleaf.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityThymeleafSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityThymeleafSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring/security/thymeleaf/spring/boot/SpringSecurityThymeleafSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.security.thymeleaf.spring.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityThymeleafSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
